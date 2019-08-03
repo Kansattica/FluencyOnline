d864ba0c4837a25ac5dbb9280e7074e52c328855
Grace H <Grace@hypnovir.us>
2019-08-03 03:58:13 -0700

    Don't include the examples that run forever and overflow the stack

--- Models/ExampleImporter.cs
+++ Models/ExampleImporter.cs
@@ -10,10 +10,14 @@ namespace FluencyOnline.Models
     public class ExampleImporter : ComponentBase
     {
         protected static List<(string Filename, string Code)> examples;
+
+        private static readonly HashSet<string> DontInclude = new HashSet<string>()
+        {"FluencyOnline.countup.fl", "FluencyOnline.excited.fl"};
         public ExampleImporter()
         {
             var assembly = typeof(ExampleImporter).GetTypeInfo().Assembly;
             examples = assembly.GetManifestResourceNames()
+            .Where(x => !DontInclude.Contains(x))
             .Select(x => (x.Substring(14),ReadEmbeddedFile(assembly.GetManifestResourceStream(x)))) //remove the 14 character long FluencyOnline. at the start of each key
             .ToList();
             examples.Insert(0, ("Pick an example", " "));
